dcf0066742b137892666519d04265df86386d15c
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-09-23 13:49:44 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    const buffer = [];
+
+    first.split('').reverse().forEach((top_digit, top_index) =>
+        second.split('').reverse().forEach((bottom_digit, bottom_index) => {
+            const [cur_multiply, cur_rank] = [top_digit * bottom_digit, top_index + bottom_index];
+            (cur_rank in buffer)
+                ? buffer[cur_rank] += cur_multiply
+                : buffer[cur_rank] = cur_multiply;
+        })
+    );
+    buffer.forEach((cur_multiply, index) => {
+        const [, memory, ones] = cur_multiply.toString().match(/(.*)(\d)$/);
+        buffer[index] = ones;
+        if (memory) {
+            ((index + 1) in buffer)
+            ? buffer[index + 1] += Number(memory)
+            : buffer.push(Number(memory));
+        }
+    });
+    return buffer.reverse().join('');
+};
